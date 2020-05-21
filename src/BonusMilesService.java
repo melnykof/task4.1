7f4fcb8467787bb0921e77e9e0d4290546d68a09
Melnykov Artem <melnykofart@gmail.com>
2020-05-21 15:42:44 +0300

    frist commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = 10_000;
+        int miles = price / 20;
+        System.out.println(cost);
+        return miles;
+    }
+        }
+
+
+
